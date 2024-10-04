7e0d96bfb3754e63e9fb2eb7f4bc583d62de5d61
korawit <korawit.orkphol@gmail.com>
2024-10-04 16:46:59 +0000

    first version of rechart demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import './App.css';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Label } from 'recharts';
+
+function generateRandomData() {
+  const startDate = new Date('2024-02-14T00:00:00Z'); // Start date
+  const endDate = new Date('2024-02-15T00:00:00Z'); // End date (two days later)
+  const intervalMinutes = 15; // Time interval in minutes
+
+  const data = [];
+  let currentTime = startDate;
+
+  while (currentTime <= endDate) {
+      const timestamp = currentTime.toISOString(); // Convert to ISO string
+      const x = Math.random() // Random value for x
+
+      data.push({ timestamp, x });
+
+      // Increment time by the specified interval
+      currentTime = new Date(currentTime.getTime() + intervalMinutes * 60 * 1000);
+  }
+
+  return data;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+  
+  const data = generateRandomData();
+  return (<>
+    <LineChart width={1000} height={300} data={data}>
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="timestamp" />
+    <YAxis yAxisId="left">
+        <Label value="mm/s" position="insideLeft" angle={-90} />
+    </YAxis>
+    <Tooltip />
+    <Legend />  
+    <Line type="monotone" dataKey="x" stroke="#8884d8" yAxisId="left" dot={false} />
+    </LineChart></>
   );
 }
 
